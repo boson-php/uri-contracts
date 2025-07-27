3207cbbb6d55570d34a76ea8bc0060219307eb70
SerafimArts <nesk@xakep.ru>
2025-07-27 21:32:28 +0000

    Add URI contracts tests

---
+++ src/Component/PathInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Contracts\Uri\Component;
+
+use Boson\Contracts\Uri\UriInterface;
+use Boson\Contracts\ValueObject\StringValueObjectInterface;
+
+/**
+ * Represents the path component of an {@see UriInterface}.
+ *
+ * @link https://datatracker.ietf.org/doc/html/rfc3986#section-3.3
+ *
+ * @template-extends \Traversable<array-key, non-empty-string>
+ */
+interface PathInterface extends
+    StringValueObjectInterface,
+    \Traversable,
+    \Countable
+{
+    /**
+     * @return int<0, max>
+     */
+    public function count(): int;
+}
---
+++ tests/CompatibilityTest.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Contracts\Uri\Tests;
+
+use Boson\Contracts\Uri\Component\AuthorityInterface;
+use Boson\Contracts\Uri\Component\PathInterface;
+use Boson\Contracts\Uri\Component\QueryInterface;
+use Boson\Contracts\Uri\Component\SchemeInterface;
+use Boson\Contracts\Uri\Component\UserInfoInterface;
+use Boson\Contracts\Uri\UriInterface;
+use PHPUnit\Framework\Attributes\DoesNotPerformAssertions;
+use PHPUnit\Framework\Attributes\Group;
+
+/**
+ * Note: Changing the behavior of these tests is allowed ONLY when updating
+ *       a MAJOR version of the package.
+ */
+#[Group('boson-php/uri-contracts')]
+final class CompatibilityTest extends TestCase
+{
+    #[DoesNotPerformAssertions]
+    public function testUriInterfaceCompatibility(): void
+    {
+        new class implements UriInterface {
+            public ?SchemeInterface $scheme {
+                get {
+                }
+            }
+            public ?AuthorityInterface $authority {
+                get {
+                }
+            }
+            public PathInterface $path {
+                get {
+                }
+            }
+            public QueryInterface $query {
+                get {
+                }
+            }
+            public ?string $fragment {
+                get {
+                }
+            }
+
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testSchemeInterfaceCompatibility(): void
+    {
+        new class implements SchemeInterface {
+            public string $name {
+                get {
+                }
+            }
+
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testAuthorityInterfaceCompatibility(): void
+    {
+        new class implements AuthorityInterface {
+            public ?UserInfoInterface $userInfo {
+                get {
+                }
+            }
+            public string $host {
+                get {
+                }
+            }
+            public ?int $port {
+                get {
+                }
+            }
+
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testPathInterfaceCompatibility(): void
+    {
+        new class implements PathInterface, \IteratorAggregate {
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+
+            public function count(): int {}
+
+            public function getIterator(): \Traversable {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testQueryInterfaceCompatibility(): void
+    {
+        new class implements QueryInterface, \IteratorAggregate {
+            public function has(string $key): bool {}
+
+            public function get(string $key, ?string $default = null): ?string {}
+
+            public function int(string $key, ?int $default = null): ?int {}
+
+            public function array(string $key, array $default = []): array {}
+
+            public function toArray(): array {}
+
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+
+            public function count(): int {}
+
+            public function getIterator(): \Traversable {}
+        };
+    }
+
+    #[DoesNotPerformAssertions]
+    public function testUserInfoInterfaceCompatibility(): void
+    {
+        new class implements UserInfoInterface {
+            public string $user {
+                get {
+                }
+            }
+            public ?string $password {
+                get {
+                }
+            }
+
+            public function equals(mixed $other): bool {}
+
+            public function __toString(): string {}
+
+            public function toString(): string {}
+        };
+    }
+}
